7d0412892e0613bf5af92f392f0307d9004d43b0
Ananya Rastogi <122351997+ananyarastogi2004@users.noreply.github.com>
2024-07-04 20:18:45 +0530

    Create expenses_list.dart

---
+++ expenses_list/expenses_list.dart
@@ -0,0 +1,28 @@
+import 'package:expense_tracker_app/expenses_list/expenses_item.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
+
+  final void Function(Expense expense) onRemoveExpense;
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: expenses.length,
+        itemBuilder: (context, index) => Dismissible(
+            key: ValueKey(expenses[index]),
+            background: Container(
+              color: Theme.of(context).colorScheme.error.withOpacity(0.50),
+              margin: EdgeInsets.symmetric(
+                horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+            ),
+            onDismissed: (direction) {
+              onRemoveExpense(expenses[index]);
+            },
+            child: ExpensesItem(expenses[index])));
+  }
+}
